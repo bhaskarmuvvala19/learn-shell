57f67e6490db90b2ca70f90e0a37d91700e06b73
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-02 17:14:10 +0530

    inputs

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,2 +1,10 @@
-echo $1
-echo $2
+# 0-n , $*/$@, $# special variables we cannot declare
+echo First Argument $1 # First Argument
+echo Second Argument $2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
+
+echo Number of Arguments = $# # Number of Arguments
