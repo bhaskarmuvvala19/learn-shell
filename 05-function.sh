e5ef0e62760114e91a984360c863a1e8b78f466d
Bhaskar <muvvalabhaskar96@gmail.com>
2023-03-03 17:42:01 +0530

    created function with arguments

--- 05-function.sh
+++ 05-function.sh
@@ -1,8 +1,18 @@
+## Declare a function
+
 xyz() {
   echo Hello from Function
   echo First Argument - $1
   echo Second Argument -- $2
   echo All Arguments - $*
   echo No of Arguments - $#
-  }
-xyz 12 32
+  a=300
+  echo Value of a = $a
+  b=200
+}
+
+## Main Program
+## Call a function
+a=120
+xyz 123 456
+echo Value of b - $b
